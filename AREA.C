a7e35607a74c81eea96aa245723ad0e72332841d
Vikas Tiwari <154304925+VikasTiw01@users.noreply.github.com>
2024-09-20 12:18:48 +0530

    Add files via upload

---
+++ AREA.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main(){
+clrscr();
+int a=10;
+int b=5;
+
+printf("Area of rectangle is %d \n", a*b);
+printf("Area ofsquare is %d \n", a*a);
+printf("Perimeter of rectangle is %d \n", 2*(a+b));
+printf("Perimeter of square is %d \n", 2*a);
+getch();
+}
+
+
